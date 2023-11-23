10c78c82b2f21cae6d3e399fda77beba0f8f144f
GGTaxo <gg.taxo@windowslive.com>
2023-11-23 11:00:35 +0200

    Initial Commit

---
+++ webapp.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import streamlit as st
+
+image = st.camera_input("Show QR code")
+
+def cross_check_name(qr_value):
+    fixed_value = "Akrolithos_Gala_2023"
+    if qr_value == fixed_value:
+        return True
+
+
+
+if image is not None:
+    bytes_data = image.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+    detector = cv2.QRCodeDetector()
+
+    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
+
+    st.write(data)
+
+    if (cross_check_name(data)):
+        st.info("PASS")
+    else:
+        st.info("FAIL")
+
+
+
